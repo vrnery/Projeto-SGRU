1bd1cfd20b3a2ecf8a4d89625f9fec3b5cb48b2b
vrnery <vrnery@restinga.ifrs.edu.br>
2016-04-18 00:26:00 -0300

    Esta descontando o valor do almoço do cartão e invalidando o ticket com a data de utilização
    Merge master into HEAD
    
    Conflicts:
    	web/index.html

--- Implementacao/SGRU/src/main/java/NewClass2.java
+++ Implementacao/SGRU/src/main/java/NewClass2.java
@@ -60,7 +60,7 @@ public class NewClass2 {
         Ticket ticket = new Ticket();
         TicketDAO daoTicket;
         int forma;
-        
+
         do {
             System.out.println("Forma de Pagamento\r 1 - Cartão\t 2 - Ticket");
             forma = ler.nextInt();
@@ -69,92 +69,83 @@ public class NewClass2 {
             int codigo = ler.nextInt();
 
             if (forma == 1) {
+                //pesquisa o aluno pela matricula
                 daoAluno = new AlunoDAO();
                 aluno = daoAluno.carregar(String.valueOf(codigo));
                 daoAluno.encerrar();
+
+                //verifica o valor correto de acordo com a data da recarga
                 daoValor = new ValorAlmocoDAO();
                 valor = daoValor.almocoCartao(aluno.getCartao().getDataExpiracao());
                 daoValor.encerrar();
-                daoAluno = new AlunoDAO();
-                aluno.getCartao().setDebitar(valor.getValorAlmoco());
-                daoAluno.salvar(aluno);
-                daoAluno.encerrar();
+
+                //verifica se tem saldo no cartão
                 if (valor.getValorAlmoco() < aluno.getCartao().getSaldo()) {
                     txtForma = "Cartão";
+
+                    //debita do saldo o valor correto do almoço
+                    daoAluno = new AlunoDAO();
+                    aluno.getCartao().setDebitar(valor.getValorAlmoco());
+                    daoAluno.salvar(aluno);
+                    daoAluno.encerrar();
                 }
             } else {
+                //perquisa o ticket pelo codigo
                 daoTicket = new TicketDAO();
                 ticket = daoTicket.usarTicket(codigo);
                 daoTicket.encerrar();
+
+                //verifica se retornou um ticket valido
                 if (ticket != null) {
                     txtForma = "Ticket";
+
+                    //registra que o ticket foi utilizado na data
+                    daoTicket = new TicketDAO();
+                    ticket.setDataUtilizado(new Date());
+                    daoTicket.salvar(ticket);
+                    daoTicket.encerrar();
                 }
             }
-        } while (txtForma.equals(""));
-        
+        } while (txtForma.equals("")); //verifica se tem alguma forma de pagamento
+
         //Realizar a venda do almoço
         List<VendaAlmoco> venda = new ArrayList();
-
         caixa.setVendaAlmoco(venda);
-
-        caixa.getVendaAlmoco()
-                .add(new VendaAlmoco());
-        caixa.getVendaAlmoco()
-                .get(0).setFormaPagamento(txtForma);
+        caixa.getVendaAlmoco().add(new VendaAlmoco());
+        caixa.getVendaAlmoco().get(0).setFormaPagamento(txtForma);
         if (forma == 1) {
             caixa.getVendaAlmoco().get(0).setCartao(aluno.getCartao());
         } else {
             caixa.getVendaAlmoco().get(0).setTicket(ticket);
         }
 
-        caixa.getVendaAlmoco()
-                .get(0).setValorAlmoco(valor);
-        caixa.getVendaAlmoco()
-                .get(0).setCaixaRU(caixa);
-        System.out.println(
-                "Valor para cartão: " + caixa.getVendaAlmoco().get(0).getValorAlmoco().getValorAlmoco());
-
+        caixa.getVendaAlmoco().get(0).setValorAlmoco(valor);
+        caixa.getVendaAlmoco().get(0).setCaixaRU(caixa);
+        System.out.println("Valor para cartão: " + caixa.getVendaAlmoco().get(0).getValorAlmoco().getValorAlmoco());
         daoCaixa = new CaixaRUDAO();
-
         daoCaixa.salvar(caixa);
-
         daoCaixa.encerrar();
 
         //Emitir nota
-        System.out.println(
-                "\r------------------------------\r\r");
-        System.out.println(
-                "   Sistema Gerenciamento RU");
-        System.out.println(
-                "Rua Rua do IFRS");
-        System.out.println(
-                "CEP: " + 1234 + "\tFone: " + op.getTelefone());
-        System.out.println(
-                "CNPJ: " + 4321 + "\tIE: " + 5678);
-        System.out.println(
-                new Date() + "\tCodigo: " + venda.get(0).getId());
-        System.out.println(
-                "\tCupom Fiscal");
-        System.out.println(
-                "Item\t\t\tValor");
-        System.out.println(
-                "Almoço\t\t\t R$ " + valor.getValorAlmoco());
-        System.out.println(
-                "------------------------------");
-        System.out.println(
-                "Total\t\t\t R$ " + valor.getValorAlmoco());
+        System.out.println("\r------------------------------\r\r");
+        System.out.println("   Sistema Gerenciamento RU");
+        System.out.println("Rua Rua do IFRS");
+        System.out.println("CEP: " + 1234 + "\tFone: " + op.getTelefone());
+        System.out.println("CNPJ: " + 4321 + "\tIE: " + 5678);
+        System.out.println(new Date() + "\tCodigo: " + venda.get(0).getId());
+        System.out.println("\tCupom Fiscal");
+        System.out.println("Item\t\t\tValor");
+        System.out.println("Almoço\t\t\t R$ " + valor.getValorAlmoco());
+        System.out.println("------------------------------");
+        System.out.println("Total\t\t\t R$ " + valor.getValorAlmoco());
         System.out.println(venda.get(0).getFormaPagamento() + "\t R$ " + valor.getValorAlmoco());
-        if (aluno
-                != null) {
+        if (aluno != null) {
             System.out.println("Cliente: " + aluno.getNome());
         } else {
             System.out.println("Cliente: Visitante");
         }
 
-        System.out.println(
-                "Operador: " + op.getId() + " " + op.getNome());
-        System.out.println(
-                new Date());
-
+        System.out.println("Operador: " + op.getId() + " " + op.getNome());
+        System.out.println(new Date());
     }
 }
