cee1b5ecd4bc3f0fa809b24eefdfe906bef5ca09
olecramsl <olecramsl@yahoo.com.br>
2016-04-29 16:02:35 -0300

    Inclusão
    Arquivo com inserts para as tabelas de cadastro

---
+++ Banco de Dados/Cadastro.sql
@@ -0,0 +1,36 @@
+use sgru;
+
+insert into Pessoa values(1,'123456','Operador','oper1@email.com','5155555555','oper1','oper');
+insert into Pessoa values(2,'123','Cassiane','cassiane@email.com','5144444444','cassiane','123');
+insert into Pessoa values(3,'456','Marcelo','marcelo@email.com','5133333333','marcelo','456');
+insert into Pessoa values(4,'789','Vanderson','vanderson@email.com','5122222222','vanderson','789');
+
+insert into OperadorCaixa values(1);
+
+insert into cartao values(1,'2016-04-29',0);
+insert into cartao values(2,'2016-04-29',0);
+insert into cartao values(3,'2016-04-29',0);
+
+insert into Aluno values(2, 'C\\Fotos',1);
+insert into Aluno values(3, 'C\\Fotos',2);
+insert into Aluno values(4, 'C\\Fotos',3);
+
+insert into recarga values(1,100,'2016-04-14',0,1);
+insert into recarga values(2,200,'2016-04-14',0,1);
+insert into recarga values(3,300,'2016-04-15',0,1);
+insert into recarga values(4,400,'2016-04-16',0,1);
+
+insert into recarga values(5,100,'2016-04-19',0,2);
+insert into recarga values(6,200,'2016-04-17',0,2);
+insert into recarga values(7,300,'2016-04-18',0,2);
+insert into recarga values(8,400,'2016-04-16',0,2);
+
+insert into recarga values(9,100,'2016-04-29',0,3);
+insert into recarga values(10,200,'2016-04-29',0,3);
+insert into recarga values(11,300,'2016-04-29',0,3);
+insert into recarga values(12,400,'2016-04-29',0,3);
+
+insert into valoralmoco values(1,1,'2016-04-14');
+insert into valoralmoco values(2,2,'2016-04-15');
+insert into valoralmoco values(3,3,'2016-04-16');
+insert into valoralmoco values(4,4,'2016-04-18');
