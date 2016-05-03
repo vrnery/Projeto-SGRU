d336f8a023dee10fc48d43ef21e788a5ec484183
olecramsl <olecramsl@yahoo.com.br>
2016-05-03 15:40:23 -0300

    Cadastros
    Inclusão de um professor
    Inclusão do caminho das fotos

--- Banco de Dados/Cadastro.sql
+++ Banco de Dados/Cadastro.sql
@@ -4,16 +4,19 @@ insert into Pessoa values(1,'123456','Operador','oper1@email.com','5155555555','
 insert into Pessoa values(2,'123','Cassiane','cassiane@email.com','5144444444','cassiane','123');
 insert into Pessoa values(3,'456','Marcelo','marcelo@email.com','5133333333','marcelo','456');
 insert into Pessoa values(4,'789','Vanderson','vanderson@email.com','5122222222','vanderson','789');
+insert into Pessoa values(5,'987','Leonardo','leonardo@email.com','5111111111','leonardo','987');
 
 insert into OperadorCaixa values(1);
 
 insert into cartao values(1,'2016-04-29',0);
 insert into cartao values(2,'2016-04-29',0);
 insert into cartao values(3,'2016-04-29',0);
+insert into cartao values(4,'2016-04-29',0);
 
-insert into Aluno values(2, 'C\\Fotos',1);
-insert into Aluno values(3, 'C\\Fotos',2);
-insert into Aluno values(4, 'C\\Fotos',3);
+insert into Aluno values(2, 'C\\imagens\\123.jpg',1);
+insert into Aluno values(3, 'C\\imagens\\456.jpg',2);
+insert into Aluno values(4, 'C\\imagens\\789.jpg',3);
+insert into Professor values(5, 'C\\imagens\\987.jpg',4);
 
 insert into recarga values(1,100,'2016-04-14',0,1);
 insert into recarga values(2,200,'2016-04-14',0,1);
@@ -29,6 +32,11 @@ insert into recarga values(9,100,'2016-04-29',0,3);
 insert into recarga values(10,200,'2016-04-29',0,3);
 insert into recarga values(11,300,'2016-04-29',0,3);
 insert into recarga values(12,400,'2016-04-29',0,3);
+
+insert into recarga values(1,100,'2016-04-14',0,4);
+insert into recarga values(2,200,'2016-04-14',0,4);
+insert into recarga values(3,300,'2016-04-15',0,4);
+insert into recarga values(4,400,'2016-04-16',0,4);
 
 insert into valoralmoco values(1,1,'2016-04-14');
 insert into valoralmoco values(2,2,'2016-04-15');
